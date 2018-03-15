c35365c8fc7ef3b6b726d7470f1a07e3df7ce74c
Your Name <jenathkanagesan@gmail.com>
2018-03-15 12:17:50 +0000

    relate #5 write initial test for api

--- logic.js
+++ logic.js
@@ -1,39 +1,49 @@
-// var moviesKey = config.movies_key; 
+// var moviesKey = config.movies_key;
 
 
 // //queries APIs
-// var url = "https://api.themoviedb.org/3/search/movie?api_key=" + moviesKey + "&query=toy+story"; 
+// var url = "https://api.themoviedb.org/3/search/movie?api_key=" + moviesKey + "&query=toy+story";
 
 // function makeRequest (url, callback){
-// var xhr = new XMLHttpRequest(); 
+// var xhr = new XMLHttpRequest();
 
 // xhr.addEventListener('load', function(){
 //     if (xhr.status === 200){
-//      var response = JSON.parse(xhr.responseText); 
-//      callback(response); 
+//      var response = JSON.parse(xhr.responseText);
+//      callback(response);
 //     }
 //     else {
-//         console.log("Status code" + xhr.status); 
+//         console.log("Status code" + xhr.status);
 //     }
 // })
 
-// xhr.open("GET", url); 
+// xhr.open("GET", url);
 // xhr.send();
 // }
 
 //get movie details
 function movieDetails (response){
 
-    return response; 
+    return response;
 
 }
 
-
-module.exports = {movieDetails: movieDetails};
-
-
-
+function getActorInfo(response) {
+  var pagesObject =  response.query.pages;
+  return pagesObject[Object.keys(pagesObject)[0]];
 
 
-// //get actors names 
+}
+
+module.exports = {movieDetails: movieDetails,
+  getActorInfo:getActorInfo
+}
+
+
+
+
+
+
+
+// //get actors names
 // https://api.themoviedb.org/3/movie/19940/credits?api_key=9fb40ebd76d0b9d8c8904e47eb597820
