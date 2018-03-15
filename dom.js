94ca545cba2c91274c49c563490d514c6835c361
Jennah <contactjennah@gmail.com>
2018-03-15 17:03:05 +0000

    Fix errors on master branch

--- dom.js
+++ dom.js
@@ -1,41 +1,6 @@
 var logic = logic; 
 
-
+var moviesKey = config.movies_key; 
 var button = document.querySelector("button"); 
 var form = document.querySelector("form"); 
-
-
-button.addEventListener("click", buttonClicked); 
-
-
-function buttonClicked(){
-
-    console.log("preventing default"); 
-
-    var moviesKey = config.movies_key; 
-
-    var url = "https://api.themoviedb.org/3/search/movie?api_key=" + moviesKey + "&query=toy+story"; 
-   
-   var something = apiRequests.makeRequest(url, apiRequests.movieDetails); 
-   })
-
-console.log(something); 
-}; 
-
-
-
-// function movieSearch(){
-//     var moviesKey = config.movies_key; 
-
-//     var url = "https://api.themoviedb.org/3/search/movie?api_key=" + moviesKey + "&query=toy+story"; 
-  
-//      var result = logic.makeRequest(url, logic.movieDetails);
-  
-//     console.log(result);
-// }
-  
-
-  
-// //  console.log(result);  
-  
-
+var url = "https://api.themoviedb.org/3/search/movie?api_key=" + moviesKey + "&query=toy+story"; 
