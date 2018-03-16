371d7cfbce17bfbd1cf5876a664c2ecfbf2b13ce
Giulia <teggi.giulia@gmail.com>
2018-03-16 11:46:26 +0000

    add error message on empty result and delete commented out text Relates #33

--- logic.js
+++ logic.js
@@ -20,7 +20,7 @@ var apiRequests = {
   },
 
   movieDetails: function (response, cb) {
-
+     
     // cuts input array length to maximum of 5 items
     var result = response.results;
     if (result.length > 4) {
@@ -33,8 +33,9 @@ var apiRequests = {
       innerObject.title = item.title;
       innerObject.poster_path = item.poster_path;
       innerObject.overview = item.overview;
-      innerObject.release_date = item.release_date;
+      innerObject.release_date = item.release_date; 
       return innerObject;
+      
     })
     )
   },
